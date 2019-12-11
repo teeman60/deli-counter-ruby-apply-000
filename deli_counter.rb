e5f337f538f9bf4631e37723e1c53ec80116a5da
Taiye Salami <taiye41@gmail.com>
2019-12-11 21:37:58 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,30 +1,91 @@
 katz_deli = []
 
-def take_a_number(katz_deli, name)
-  katz_deli.push("#{name}")
-  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
-end
-
-
-def line(katz_deli)
-   array = []
-  if katz_deli.length == 0
+def line(deli)
+  array = []
+  if deli.length == 0
     puts "The line is currently empty."
   else
-    katz_deli.each.with_index(1) do |name, i|
-      array.push("#{i}. #{name}")
-      
+    deli.each.with_index(1) do |name, idx|
+      array << "#{idx}. #{name}"
     end
     puts "The line is currently: #{array.join(" ")}"
   end
 end
 
 
+def take_a_number(deli, name)
+  deli.push(name)
+  puts "Welcome, #{name}. You are number #{deli.size} in line."
+end
+
+
 def now_serving(katz_deli)
-  if katz_deli.length == 0
+  if katz_deli.size == 0
     puts "There is nobody waiting to be served!"
   else
     puts "Currently serving #{katz_deli[0]}."
-  end
   katz_deli.shift
+  end
+  
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# katz_deli = []
+
+# def take_a_number(katz_deli, name)
+#   katz_deli.push("#{name}")
+#   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+# end
+
+
+# def line(katz_deli)
+#   array = []
+#   if katz_deli.length == 0
+#     puts "The line is currently empty."
+#   else
+#     katz_deli.each.with_index(1) do |name, i|
+#       array.push("#{i}. #{name}")
+      
+#     end
+#     puts "The line is currently: #{array.join(" ")}"
+#   end
+# end
+
+
+# def now_serving(katz_deli)
+#   if katz_deli.length == 0
+#     puts "There is nobody waiting to be served!"
+#   else
+#     puts "Currently serving #{katz_deli[0]}."
+#   end
+#   katz_deli.shift
+# end
